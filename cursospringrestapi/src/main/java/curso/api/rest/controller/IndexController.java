5b54caacebfae1c2bbb6879df997d1f258067608
MarcellaCanedo <marcellacanedo@hotmail.com>
2020-01-16 14:32:19 -0300

    novos usuarios e telefones - endpoint

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,18 +1,18 @@
 package curso.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
@@ -42,6 +42,21 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
+
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		return "ok";		
+	}
+	
+	//deletar todas as vendas de um usuário
+	@DeleteMapping(value = "/{id}/venda", produces = "application/text")
+	public String deleteVenda(@PathVariable("id") Long id) {
+
+		usuarioRepository.deleteById(id);
+		return "ok";
+	}
 	
 	/*metodo para consultar todos*/
 	@GetMapping(value = "/", produces = "application/json")
@@ -50,11 +65,14 @@ public class IndexController {
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 	
-	@PostMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+		
+		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
 		
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
-		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
 	
