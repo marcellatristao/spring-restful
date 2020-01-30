aa0423f326f6e529a4de18acd24d26a75a90583e
MarcellaCanedo <marcellacanedo@hotmail.com>
2020-01-30 16:06:13 -0300

    Capturando erros do backend

--- cursospringrestapi/src/main/java/curso/api/rest/ControleExcecoes.java
+++ cursospringrestapi/src/main/java/curso/api/rest/ControleExcecoes.java
@@ -1,6 +1,7 @@
 package curso.api.rest;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.postgresql.util.PSQLException;
@@ -8,6 +9,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +26,17 @@ public class ControleExcecoes extends ResponseEntityExceptionHandler{
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
+		
+		String msg = "";
+		
+		if(ex instanceof MethodArgumentNotValidException) {
+			List<ObjectError>list = ((MethodArgumentNotValidException)ex).getBindingResult().getAllErrors();
+			for (ObjectError objectError : list) {
+				msg += objectError.getDefaultMessage() + "\n";
+			}
+		}else{
+			msg = ex.getMessage();
+		}
 		
 		ObjetoError objetoError = new ObjetoError();
 		objetoError.setError(ex.getMessage());
